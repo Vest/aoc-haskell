027f52a5cebb45819fb2f9451ce66e882c869ab7
Vest <Vest@users.noreply.github.com>
2021-10-11 19:17:01 +0200

    Day 1 / 2016: 239

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,105 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+    parseInput,
+    parseStep,
+    Rotate (Right, Left),
+    Direction (North, South, East, West),
+    rotate,
+    Position (..),
+    moveSteps,
+    movement,
+    solution1,
+  )
+where
+
+import Data.List.Split
+import Data.Maybe
+import Debug.Trace
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Rotate = Right Int | Left Int
+
+data Position = Position
+  { row,
+    col ::
+      Int
+  }
+
+data Direction = North | South | East | West
+
+instance Eq Rotate where
+  Lib.Right s1 == Lib.Right s2 = s1 == s2
+  Lib.Left s1 == Lib.Left s2 = s1 == s2
+  _ == _ = False
+
+instance Show Rotate where
+  show (Lib.Right steps) = "R" ++ show steps
+  show (Lib.Left steps) = "L" ++ show steps
+
+instance Eq Direction where
+  North == North = True
+  South == South = True
+  East == East = True
+  West == West = True
+  _ == _ = False
+
+instance Show Direction where
+  show (North) = "N"
+  show (South) = "S"
+  show (East) = "E"
+  show (West) = "W"
+
+instance Eq Position where
+  p1 == p2 = row p1 == row p2 && col p1 == col p2
+
+instance Show Position where
+  show (Lib.Position row col) = "(" ++ (show col) ++ ", " ++ (show row) ++ ")"
+
+parseStep :: String -> Maybe Rotate
+parseStep stepString = case rotationChar of
+  'R' -> Just . Lib.Right $ steps
+  'L' -> Just . Lib.Left $ steps
+  _ -> Nothing
+  where
+    rotationChar = head stepString
+    steps = read . tail $ stepString :: Int
+
+parseInput :: String -> [Rotate]
+parseInput = catMaybes . map parseStep . split (dropDelims . dropBlanks $ onSublist ", ")
+
+rotate :: Rotate -> Direction -> Direction
+rotate rot dir = case (rot, dir) of
+  (Lib.Right _, North) -> East
+  (Lib.Right _, South) -> West
+  (Lib.Right _, East) -> South
+  (Lib.Right _, West) -> North
+  (Lib.Left _, North) -> West
+  (Lib.Left _, South) -> East
+  (Lib.Left _, East) -> North
+  (Lib.Left _, West) -> South
+
+moveSteps :: Int -> (Direction, Position) -> (Direction, Position)
+moveSteps steps (North, pos) = (North, pos {row = row pos + steps})
+moveSteps steps (East, pos) = (East, pos {col = col pos + steps})
+moveSteps steps (West, pos) = (West, pos {col = col pos - steps})
+moveSteps steps (South, pos) = (South, pos {row = row pos - steps})
+
+movement :: (Direction, Position) -> [Rotate] -> (Direction, Position)
+movement (dir, pos) [] = (dir, pos)
+movement (dir, pos) (x : xpath) =
+  let newDir = rotate x dir
+      steps = case x of
+        Lib.Right s -> s
+        Lib.Left s -> s
+      newPos = moveSteps steps (newDir, pos)
+   in movement newPos xpath
+
+findPath :: Position -> Int
+findPath (Position {row, col}) = abs row + abs col
+
+solution1 :: String -> Int
+solution1 input = findPath . snd . movement (North, Position 0 0) . parseInput $ input
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,34 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib
+import Test.HUnit
+
+foo :: Int -> (Int, Int)
+foo x = (1, x)
+
+test1 :: Test
+test1 = TestCase (assertEqual "for (foo 3)," (1, 3) (foo 3))
+
+test2 :: Test
+test2 = TestCase (assertEqual "parseStep(R1)" (Just (Lib.Right 1)) (Lib.parseStep "R1"))
+
+tests :: Test
+tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+
+tests' :: Test
+tests' =
+  test
+    [ "day1" ~: "parseStep(R1)" ~: Just (Lib.Right 1) ~=? Lib.parseStep "R1",
+      "day1" ~: "parseStep(R2)" ~: Just (Lib.Right 2) ~=? Lib.parseStep "R2",
+      "day1" ~: "parseStep(L1)" ~: Just (Lib.Left 1) ~=? Lib.parseStep "L1",
+      "day1" ~: "parseStep(L2)" ~: Just (Lib.Left 2) ~=? Lib.parseStep "L2",
+      "day1" ~: "parseInput('R1, L2')" ~: [Lib.Right 1, Lib.Left 2] ~=? Lib.parseInput "R1, L2",
+      "day1" ~: "rotate Rx" ~: [Lib.East, Lib.West, Lib.North, Lib.South] ~=? map (Lib.rotate (Lib.Right 3)) [Lib.North, Lib.South, Lib.West, Lib.East],
+      "day1" ~: "rotate Lx" ~: [Lib.West, Lib.East, Lib.South, Lib.North] ~=? map (Lib.rotate (Lib.Left 3)) [Lib.North, Lib.South, Lib.West, Lib.East],
+      "day1" ~: "moveSteps a circle" ~: (West, Position 0 0) ~=? Lib.moveSteps 5 (West, snd (Lib.moveSteps 5 (South, snd (Lib.moveSteps 5 (East, snd (Lib.moveSteps 5 (North, Position 0 0))))))),
+      "day1" ~: "movement(R2, L3)" ~: (North, Position 3 2) ~=? Lib.movement (North, Position 0 0) [Lib.Right 2, Lib.Left 3],
+      "day1" ~: "solution1('R5, L5, R5, R3')" ~: 12 ~=? Lib.solution1 "R5, L5, R5, R3"
+    ]
+
+main :: IO Counts
+main = do
+  _ <- runTestTT tests
+  runTestTT tests'
