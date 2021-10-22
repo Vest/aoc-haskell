5f178471c4d81915fe99a835f49f0ef65ef7b8ee
Vest <Vest@users.noreply.github.com>
2021-10-22 18:59:00 +0200

    Day 2 / 2016: 69642 Tests are incomplete (they fail for "border" values) Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day2 where
 
+import qualified Data.List as L
+
+data Direction = U | D | L | R | Empty
+  deriving (Eq, Show)
+
+{-1 2 3
+  4 5 6
+  7 8 9-}
+step :: Int -> Direction -> Int
+step num U | num > 3 = num - 3
+step num D | num < 7 = num + 3
+step num L | num `notElem` [1, 4, 7] = num - 1
+step num R | num `notElem` [3, 6, 9] = num + 1
+step num _ = num
+
+parseLine :: String -> [Direction]
+parseLine =
+  filter (/= Empty)
+    . map
+      ( \case
+          'U' -> U
+          'D' -> D
+          'L' -> L
+          'R' -> R
+          _ -> Empty
+      )
+
+detectNumber :: Int -> [Direction] -> Int
+detectNumber 0 = foldl step 5
+detectNumber n = foldl step n
+
 solution1 :: String -> Int
-solution1 _ = 1
+solution1 =
+  fst
+    . foldl
+      ( \(answer, startNum) dirs ->
+          let newNum = detectNumber startNum dirs
+           in (newNum + 10 * if (startNum `mod` 10) /= 0 then answer else startNum, newNum)
+      )
+      (5, 0)
+    . map parseLine
+    . lines
+
+solution :: String -> String
+solution input = (show . solution1 $ input)
--- test/Days/Day2.hs
+++ test/Days/Day2.hs
@@ -6,5 +6,20 @@ import Test.HUnit
 tests' :: Test
 tests' =
   test
-    [ "day2" ~: "solution1(ULL\nRRDDD\nLURDL\nUUUUD)" ~: 1985 ~=? Day2.solution1 "ULL\nRRDDD\nLURDL\nUUUUD"
+    [ "day2" ~: "solution1(ULL\nRRDDD\nLURDL\nUUUUD)" ~: 1985 ~=? Day2.solution1 "ULL\nRRDDD\nLURDL\nUUUUD",
+      "day2" ~: "step 5 U" ~: 2 ~=? Day2.step 5 U,
+      "day2" ~: "parseLine RRDDD" ~: [R, R, D, D, D] ~=? Day2.parseLine "RRDDD",
+      "day2" ~: "detectNumber 0 empty" ~: 5 ~=? Day2.detectNumber 0 [],
+      "day2" ~: "detectNumber RRR" ~: 6 ~=? Day2.detectNumber 5 [R, R, R],
+      "day2" ~: "detectNumber LLL" ~: 4 ~=? Day2.detectNumber 5 [L, L, L],
+      "day2" ~: "detectNumber UUU" ~: 2 ~=? Day2.detectNumber 5 [U, U, U],
+      "day2" ~: "detectNumber DDD" ~: 8 ~=? Day2.detectNumber 5 [D, D, D],
+      "day2" ~: "detectNumber ULL" ~: 1 ~=? Day2.detectNumber 5 [U, L, L],
+      "day2" ~: "detectNumber RRDDD" ~: 9 ~=? Day2.detectNumber 1 [R, R, D, D, D],
+      "day2" ~: "detectNumber LURDL" ~: 8 ~=? Day2.detectNumber 9 [L, U, R, D, L],
+      "day2" ~: "detectNumber UUUUD" ~: 5 ~=? Day2.detectNumber 8 [U, U, U, U, D],
+      "day2" ~: "solution1 empty" ~: 5 ~=? Day2.solution1 "",
+      "day2" ~: "solution1 empty twice" ~: 55 ~=? Day2.solution1 "\n",
+      "day2" ~: "solution1 empty thrice :)" ~: 555 ~=? Day2.solution1 "\n\n",
+      "day2" ~: "solution1 empty four times" ~: 5555 ~=? Day2.solution1 "\n\n\n"
     ]
