2391920951272afef921f8596612c033c9a8c365
Vest <Vest@users.noreply.github.com>
2021-10-26 22:16:13 +0200

    Day 2 / 2016: 69642 Refactored code to be ready for the second part. Removed unused coding, converted solution1 from Int to String
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,21 +1,48 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Day2 where
 
-import qualified Data.List as L
-
 data Direction = U | D | L | R | Empty
+  deriving (Eq, Show)
+
+data Position = Position {x, y :: Int}
   deriving (Eq, Show)
 
 {-1 2 3
   4 5 6
   7 8 9-}
-step :: Int -> Direction -> Int
-step num U | num > 3 = num - 3
-step num D | num < 7 = num + 3
-step num L | num `notElem` [1, 4, 7] = num - 1
-step num R | num `notElem` [3, 6, 9] = num + 1
-step num _ = num
+getKey :: Position -> Maybe Char
+getKey Position {x = -1, y = -1} = Just '7'
+getKey Position {x = 0, y = -1} = Just '8'
+getKey Position {x = 1, y = -1} = Just '9'
+getKey Position {x = -1, y = 0} = Just '4'
+getKey Position {x = 0, y = 0} = Just '5'
+getKey Position {x = 1, y = 0} = Just '6'
+getKey Position {x = -1, y = 1} = Just '1'
+getKey Position {x = 0, y = 1} = Just '2'
+getKey Position {x = 1, y = 1} = Just '3'
+getKey Position {x = _, y = _} = Nothing
+
+stepPosition :: Position -> Direction -> Position
+stepPosition Position {x, y} dir =
+  let newPos =
+        Position
+          { x =
+              x + case dir of
+                L -> -1
+                R -> 1
+                _ -> 0,
+            y =
+              y + case dir of
+                U -> 1
+                D -> -1
+                _ -> 0
+          }
+      newKey = getKey newPos
+   in case newKey of
+        Nothing -> Position {x, y}
+        Just _ -> newPos
 
 parseLine :: String -> [Direction]
 parseLine =
@@ -29,21 +56,24 @@ parseLine =
           _ -> Empty
       )
 
-detectNumber :: Int -> [Direction] -> Int
-detectNumber 0 = foldl step 5
-detectNumber n = foldl step n
+detectPosition :: Position -> [Direction] -> Position
+detectPosition = foldl stepPosition
 
-solution1 :: String -> Int
+solution1 :: String -> String
 solution1 =
-  fst
+  snd
     . foldl
-      ( \(answer, startNum) dirs ->
-          let newNum = detectNumber startNum dirs
-           in (newNum + 10 * if (startNum `mod` 10) /= 0 then answer else startNum, newNum)
+      ( \(startPos, answer) dirs ->
+          let newPos = detectPosition startPos dirs :: Position
+           in ( newPos,
+                case getKey newPos of
+                  Just key -> answer ++ [key]
+                  Nothing -> answer
+              )
       )
-      (5, 0)
+      (Day2.Position {x = 0, y = 0}, [])
     . map parseLine
     . lines
 
 solution :: String -> String
-solution input = (show . solution1 $ input)
+solution = solution1
--- test/Days/Day2.hs
+++ test/Days/Day2.hs
@@ -6,20 +6,19 @@ import Test.HUnit
 tests' :: Test
 tests' =
   test
-    [ "day2" ~: "solution1(ULL\nRRDDD\nLURDL\nUUUUD)" ~: 1985 ~=? Day2.solution1 "ULL\nRRDDD\nLURDL\nUUUUD",
-      "day2" ~: "step 5 U" ~: 2 ~=? Day2.step 5 U,
+    [ "day2" ~: "solution1(ULL\nRRDDD\nLURDL\nUUUUD)" ~: "1985" ~=? Day2.solution1 "ULL\nRRDDD\nLURDL\nUUUUD",
       "day2" ~: "parseLine RRDDD" ~: [R, R, D, D, D] ~=? Day2.parseLine "RRDDD",
-      "day2" ~: "detectNumber 0 empty" ~: 5 ~=? Day2.detectNumber 0 [],
-      "day2" ~: "detectNumber RRR" ~: 6 ~=? Day2.detectNumber 5 [R, R, R],
-      "day2" ~: "detectNumber LLL" ~: 4 ~=? Day2.detectNumber 5 [L, L, L],
-      "day2" ~: "detectNumber UUU" ~: 2 ~=? Day2.detectNumber 5 [U, U, U],
-      "day2" ~: "detectNumber DDD" ~: 8 ~=? Day2.detectNumber 5 [D, D, D],
-      "day2" ~: "detectNumber ULL" ~: 1 ~=? Day2.detectNumber 5 [U, L, L],
-      "day2" ~: "detectNumber RRDDD" ~: 9 ~=? Day2.detectNumber 1 [R, R, D, D, D],
-      "day2" ~: "detectNumber LURDL" ~: 8 ~=? Day2.detectNumber 9 [L, U, R, D, L],
-      "day2" ~: "detectNumber UUUUD" ~: 5 ~=? Day2.detectNumber 8 [U, U, U, U, D],
-      "day2" ~: "solution1 empty" ~: 5 ~=? Day2.solution1 "",
-      "day2" ~: "solution1 empty twice" ~: 55 ~=? Day2.solution1 "\n",
-      "day2" ~: "solution1 empty thrice :)" ~: 555 ~=? Day2.solution1 "\n\n",
-      "day2" ~: "solution1 empty four times" ~: 5555 ~=? Day2.solution1 "\n\n\n"
+      "day2" ~: "detectPosition 0 empty" ~: Position {x = 0, y = 0} ~=? Day2.detectPosition Position {x = 0, y = 0} [],
+      "day2" ~: "detectPosition RRR" ~: Position {x = 1, y = 0} ~=? Day2.detectPosition Position {x = 0, y = 0} [R, R, R],
+      "day2" ~: "detectPosition LLL" ~: Position {x = -1, y = 0} ~=? Day2.detectPosition Position {x = 0, y = 0} [L, L, L],
+      "day2" ~: "detectPosition UUU" ~: Position {x = 0, y = 1} ~=? Day2.detectPosition Position {x = 0, y = 0} [U, U, U],
+      "day2" ~: "detectPosition DDD" ~: Position {x = 0, y = -1} ~=? Day2.detectPosition Position {x = 0, y = 0} [D, D, D],
+      "day2" ~: "detectPosition ULL" ~: Position {x = -1, y = 1} ~=? Day2.detectPosition Position {x = 0, y = 0} [U, L, L],
+      "day2" ~: "detectPosition RRDDD" ~: Position {x = 1, y = -1} ~=? Day2.detectPosition Position {x = -1, y = 1} [R, R, D, D, D],
+      "day2" ~: "detectPosition LURDL" ~: Position {x = 0, y = -1} ~=? Day2.detectPosition Position {x = 1, y = -1} [L, U, R, D, L],
+      "day2" ~: "detectPosition UUUUD" ~: Position {x = 0, y = 0} ~=? Day2.detectPosition Position {x = 0, y = -1} [U, U, U, U, D],
+      "day2" ~: "solution1 empty" ~: "" ~=? Day2.solution1 "",
+      "day2" ~: "solution1 empty twice" ~: "5" ~=? Day2.solution1 "\n",
+      "day2" ~: "solution1 empty thrice :)" ~: "55" ~=? Day2.solution1 "\n\n",
+      "day2" ~: "solution1 empty four times" ~: "555" ~=? Day2.solution1 "\n\n\n"
     ]
