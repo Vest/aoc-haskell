854b432aebced3bd177f0578f3ffcec5107143ff
Vest <Vest@users.noreply.github.com>
2021-10-29 22:56:52 +0200

    Day 5 / 2016: 2414bc77, 437e60fc
    The god of Bloodborne!!!!!

--- src/Day5.hs
+++ src/Day5.hs
@@ -2,7 +2,9 @@ module Day5 where
 
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 import qualified Data.Digest.Pure.MD5 as MD5
-import Data.List (stripPrefix)
+import Data.List (find, stripPrefix)
+import qualified Data.Map as M
+import Data.Maybe
 
 getMD5 :: String -> Int -> String
 getMD5 input num =
@@ -29,4 +31,28 @@ solution1 :: String -> String
 solution1 = getPassword . head . lines
 
 solution :: String -> String
-solution = solution1
+solution input = solution1 input ++ ", " ++ solution2 input
+
+getAdvancedPassword :: String -> String
+getAdvancedPassword prefix =
+  M.elems
+    . fromMaybe M.empty
+    . find
+      (\m -> M.size m == 8)
+    $ scanl
+      ( \passMap i ->
+          let md5 = getMD5 prefix i
+              withoutPrefix = stripPrefix "00000" md5
+           in case withoutPrefix of
+                Just (pos : ltr : _)
+                  | pos `elem` ['0' .. '7'] ->
+                    if M.notMember pos passMap
+                      then M.insert pos ltr passMap
+                      else passMap
+                _ -> passMap
+      )
+      M.empty
+      [0 ..]
+
+solution2 :: String -> String
+solution2 = getAdvancedPassword . head . lines
