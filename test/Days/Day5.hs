29815e945afa19ca8d7fd2ae27720e2e5d24737d
Vest <Vest@users.noreply.github.com>
2021-10-29 22:55:01 +0200

    Day 5: 2414bc77, 437e60fc The god of Bloodborne!!!!!
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- test/Days/Day5.hs
+++ test/Days/Day5.hs
@@ -8,5 +8,6 @@ tests' =
   test
     [ "day5" ~: "solution1(abc)" ~: "18f47a30" ~=? Day5.solution1 "abc",
       "day5" ~: "getMD5(abc3231929)" ~: "00000155f8105dff7f56ee10fa9b9abd" ~=? Day5.getMD5 "abc" 3231929,
-      "day5" ~: "getPassword(abc)" ~: "18f47a30" ~=? Day5.getPassword "abc"
+      "day5" ~: "getPassword(abc)" ~: "18f47a30" ~=? Day5.getPassword "abc",
+      "day5" ~: "getAdvancedPassword(abc)" ~: "05ace8e3" ~=? Day5.getAdvancedPassword "abc"
     ]
