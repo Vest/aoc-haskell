a407f39f5a6fdb2b4ce5b1a47a9852d05622855c
Vest <Vest@users.noreply.github.com>
2021-11-03 17:41:22 +0100

    Day 8: Implement safe parsing of strings to **positive** integers.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,4 +1,39 @@
 module Day8 where
 
+import Data.List
+
+data Token = Rect | Rotate | Row | Column | Value Int
+
+getNextToken :: String -> (Token, Int)
+getNextToken code
+  | rectPrefix `isPrefixOf` code = (Rect, length rectPrefix)
+  | rotatePrefix `isPrefixOf` code = (Rotate, length rotatePrefix)
+  | rowPrefix `isPrefixOf` code = (Row, length rowPrefix)
+  | columnPrefix `isPrefixOf` code = (Column, length columnPrefix)
+  | otherwise = error $ "Invalid token at: " ++ code
+  where
+    rectPrefix = "rect "
+    rotatePrefix = "rotate "
+    rowPrefix = "row y="
+    columnPrefix = "column x="
+
+safeStringToInt :: String -> (Int, Int)
+safeStringToInt str =
+  let parsed =
+        snd $
+          foldl
+            ( \(toStop, answer) c ->
+                if toStop
+                  then (True, answer)
+                  else
+                    ( if c `elem` ['0' .. '9']
+                        then (False, answer ++ [c])
+                        else (True, answer)
+                    )
+            )
+            (False, [])
+            str
+   in (read parsed :: Int, length parsed)
+
 solution :: String -> String
 solution _ = "output"
--- test/Days/Day8.hs
+++ test/Days/Day8.hs
@@ -6,7 +6,9 @@ import Test.HUnit
 tests' :: Test
 tests' =
   test
-    [
-    "day8" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing"
-    {- "day8" ~: "solution1(5 10 25)" ~: "test" ~=? DayX.solution1 "5 10 25" -}
+    [ "day8" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing",
+      "day8" ~: "safeStringToInt(5)" ~: (5, 1) ~=? Day8.safeStringToInt "5",
+      "day8" ~: "safeStringToInt(10)" ~: (10, 2) ~=? Day8.safeStringToInt "10",
+      "day8" ~: "safeStringToInt(123b)" ~: (123, 3) ~=? Day8.safeStringToInt "123b",
+      "day8" ~: "safeStringToInt(123b1)" ~: (123, 3) ~=? Day8.safeStringToInt "123b1"
     ]
