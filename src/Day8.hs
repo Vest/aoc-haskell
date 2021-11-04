c746d0d6516974d554f347f8b7a7192258c31d8a
Vest <Vest@users.noreply.github.com>
2021-11-04 23:41:18 +0100

    Day 8 / 2016: 123
    I want to thank this resource for giving me an idea: https://dev.to/yujiri8/a-haskell-ast-parser-and-formatter-4hp5
    I didn't copy code 1-to-1, but took basic idea. Thanks again.

---
+++ src/Day8.hs
@@ -0,0 +1,112 @@
+module Day8 where
+
+import Data.Bifunctor
+import Data.List
+
+data Token = Rect | Rotate | Row | Column | Value Int | Skip
+  deriving (Eq, Show)
+
+type AST = [Statement]
+
+type Screen = [String]
+
+data Statement = Rectangle Int Int | RotateRow Int Int | RotateColumn Int Int | NOP
+  deriving (Eq, Show)
+
+parseToTokens :: [Token] -> String -> [Token]
+parseToTokens tokens "" = tokens
+parseToTokens tokens str =
+  let (token, skipToken) = getNextToken str
+   in parseToTokens (tokens ++ [token]) (drop skipToken str)
+
+getNextToken :: String -> (Token, Int)
+getNextToken code
+  | rectPrefix `isPrefixOf` code = (Rect, length rectPrefix)
+  | rotatePrefix `isPrefixOf` code = (Rotate, length rotatePrefix)
+  | rowPrefix `isPrefixOf` code = (Row, length rowPrefix)
+  | columnPrefix `isPrefixOf` code = (Column, length columnPrefix)
+  | skipX `isPrefixOf` code = (Skip, length skipX)
+  | skipBy `isPrefixOf` code = (Skip, length skipBy)
+  | snd intPrefix > 0 = first Value intPrefix
+  | otherwise = error $ "Invalid token at: " ++ code
+  where
+    rectPrefix = "rect "
+    rotatePrefix = "rotate "
+    rowPrefix = "row y="
+    columnPrefix = "column x="
+    skipX = "x"
+    skipBy = " by "
+    intPrefix = safeStringToInt code
+
+safeStringToInt :: String -> (Int, Int)
+safeStringToInt str =
+  let parsed =
+        snd
+          . foldl
+            ( \(toStop, answer) c ->
+                if toStop
+                  then (True, answer)
+                  else
+                    ( if c `elem` ['0' .. '9']
+                        then (False, answer ++ [c])
+                        else (True, answer)
+                    )
+            )
+            (False, [])
+          $ str
+   in if null parsed
+        then (0, 0)
+        else (read parsed :: Int, length parsed)
+
+parseStatement :: [Token] -> Statement
+parseStatement [Rect, Value a, Skip, Value b] = Rectangle a b
+parseStatement [Rotate, Column, Value x, Skip, Value y] = RotateColumn x y
+parseStatement [Rotate, Row, Value y, Skip, Value x] = RotateRow y x
+parseStatement _ = NOP
+
+buildAST :: String -> AST
+buildAST = map (parseStatement . parseToTokens []) . lines
+
+generateScreen :: Int -> Int -> Screen
+generateScreen cols rows = replicate rows (replicate cols '.')
+
+executeStatement :: Statement -> Screen -> Screen
+executeStatement (Rectangle width height) screen =
+  zipWith
+    ( \index line ->
+        let lineLength = length line
+         in if index > height
+              then line
+              else replicate (min width lineLength) '#' ++ drop width line
+    )
+    [1 ..]
+    screen
+executeStatement (RotateRow row shift) screen =
+  zipWith
+    ( \index line ->
+        let lineLength = length line
+         in if index == row
+              then
+                let longLine = cycle line
+                 in take lineLength (drop (lineLength - shift) longLine)
+              else line
+    )
+    [0 ..]
+    screen
+executeStatement (RotateColumn col shift) screen =
+  transpose
+    . executeStatement (RotateRow col shift)
+    . transpose
+    $ screen
+executeStatement NOP screen = screen
+
+solution1 :: Int -> Int -> String -> Int
+solution1 width height input =
+  let ast = buildAST input
+      startScreen = generateScreen width height
+   in length
+        . filter (== '#')
+        $ concat $ foldl (flip executeStatement) startScreen ast
+
+solution :: String -> String
+solution input = show $ solution1 50 6 input
---
+++ test/Days/Day8.hs
@@ -0,0 +1,31 @@
+module Days.Day8 where
+
+import Day8
+import Test.HUnit
+
+tests' :: Test
+tests' =
+  test
+    [ "day8" ~: "solution1(sampleData)" ~: 6 ~=? Day8.solution1 7 3 "rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1",
+      "day8" ~: "safeStringToInt(a)" ~: (0, 0) ~=? Day8.safeStringToInt "a",
+      "day8" ~: "safeStringToInt(5)" ~: (5, 1) ~=? Day8.safeStringToInt "5",
+      "day8" ~: "safeStringToInt(10)" ~: (10, 2) ~=? Day8.safeStringToInt "10",
+      "day8" ~: "safeStringToInt(123b)" ~: (123, 3) ~=? Day8.safeStringToInt "123b",
+      "day8" ~: "safeStringToInt(123b1)" ~: (123, 3) ~=? Day8.safeStringToInt "123b1",
+      "day8" ~: "getNextToken(rect 3x2)" ~: (Rect, 5) ~=? Day8.getNextToken "rect 3x2",
+      "day8" ~: "getNextToken(3x2)" ~: (Value 3, 1) ~=? Day8.getNextToken "3x2",
+      "day8" ~: "parseToTokens(rect 3x2)" ~: [Rect, Value 3, Skip, Value 2] ~=? Day8.parseToTokens [] "rect 3x2",
+      "day8" ~: "parseToTokens(rotate column x=1 by 1)" ~: [Rotate, Column, Value 1, Skip, Value 1] ~=? Day8.parseToTokens [] "rotate column x=1 by 1",
+      "day8" ~: "parseToTokens(rotate row y=0 by 4)" ~: [Rotate, Row, Value 0, Skip, Value 4] ~=? Day8.parseToTokens [] "rotate row y=0 by 4",
+      "day8" ~: "parseStatement(rect 3x2)" ~: Rectangle 3 2 ~=? Day8.parseStatement (Day8.parseToTokens [] "rect 3x2"),
+      "day8" ~: "parseStatement(rotate column x=1 by 1)" ~: RotateColumn 1 1 ~=? Day8.parseStatement (Day8.parseToTokens [] "rotate column x=1 by 1"),
+      "day8" ~: "parseStatement(rotate row y=0 by 4)" ~: RotateRow 0 4 ~=? Day8.parseStatement (Day8.parseToTokens [] "rotate row y=0 by 4"),
+      "day8" ~: "buildAST(sampleData)" ~: [Rectangle 3 2, RotateColumn 1 1, RotateRow 0 4, RotateColumn 1 1] ~=? Day8.buildAST "rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1",
+      "day8" ~: "generateScreen(3 2)" ~: ["...", "..."] ~=? Day8.generateScreen 3 2,
+      "day8" ~: "rect(3 2)" ~: ["###....", "###....", "......."] ~=? Day8.executeStatement (Rectangle 3 2) (Day8.generateScreen 7 3),
+      "day8" ~: "rect(0 0)" ~: [".......", ".......", "......."] ~=? Day8.executeStatement (Rectangle 0 0) (Day8.generateScreen 7 3),
+      "day8" ~: "rect(100 100)" ~: ["#######", "#######", "#######"] ~=? Day8.executeStatement (Rectangle 100 100) (Day8.generateScreen 7 3),
+      "day8" ~: "rotate column x=1 by 1" ~: ["#.#....", "###....", ".#....."] ~=? Day8.executeStatement (RotateColumn 1 1) ["###....", "###....", "......."],
+      "day8" ~: "rotate row y=0 by 4" ~: ["....#.#", "###....", ".#....."] ~=? Day8.executeStatement (RotateRow 0 4) ["#.#....", "###....", ".#....."],
+      "day8" ~: "rotate column x=1 by 1" ~: [".#..#.#", "#.#....", ".#....."] ~=? Day8.executeStatement (RotateColumn 1 1) ["....#.#", "###....", ".#....."]
+    ]
