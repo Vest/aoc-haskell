910e809604cf5c943d5ed21825fdcfc99848615e
Vest <Vest@users.noreply.github.com>
2021-11-04 13:24:28 +0100

    Day 8: Added tokenizer and safe Integer parser Contains tests as well
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day8.hs
+++ src/Day8.hs
@@ -1,8 +1,16 @@
 module Day8 where
 
+import Data.Bifunctor
 import Data.List
 
-data Token = Rect | Rotate | Row | Column | Value Int
+data Token = Rect | Rotate | Row | Column | Value Int | Skip
+  deriving (Eq, Show)
+
+parseToTokens :: String -> [Token] -> [Token]
+parseToTokens "" tokens = tokens
+parseToTokens str tokens =
+  let (token, skipToken) = getNextToken str
+   in parseToTokens (drop skipToken str) (tokens ++ [token])
 
 getNextToken :: String -> (Token, Int)
 getNextToken code
@@ -10,18 +18,24 @@ getNextToken code
   | rotatePrefix `isPrefixOf` code = (Rotate, length rotatePrefix)
   | rowPrefix `isPrefixOf` code = (Row, length rowPrefix)
   | columnPrefix `isPrefixOf` code = (Column, length columnPrefix)
+  | skipX `isPrefixOf` code = (Skip, length skipX)
+  | skipBy `isPrefixOf` code = (Skip, length skipBy)
+  | snd intPrefix > 0 = first Value intPrefix
   | otherwise = error $ "Invalid token at: " ++ code
   where
     rectPrefix = "rect "
     rotatePrefix = "rotate "
     rowPrefix = "row y="
     columnPrefix = "column x="
+    skipX = "x"
+    skipBy = " by "
+    intPrefix = safeStringToInt code
 
 safeStringToInt :: String -> (Int, Int)
 safeStringToInt str =
   let parsed =
-        snd $
-          foldl
+        snd
+          . foldl
             ( \(toStop, answer) c ->
                 if toStop
                   then (True, answer)
@@ -32,8 +46,10 @@ safeStringToInt str =
                     )
             )
             (False, [])
-            str
-   in (read parsed :: Int, length parsed)
+          $ str
+   in if null parsed
+        then (0, 0)
+        else (read parsed :: Int, length parsed)
 
 solution :: String -> String
 solution _ = "output"
--- test/Days/Day8.hs
+++ test/Days/Day8.hs
@@ -7,8 +7,14 @@ tests' :: Test
 tests' =
   test
     [ "day8" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing",
+      "day8" ~: "safeStringToInt(a)" ~: (0, 0) ~=? Day8.safeStringToInt "a",
       "day8" ~: "safeStringToInt(5)" ~: (5, 1) ~=? Day8.safeStringToInt "5",
       "day8" ~: "safeStringToInt(10)" ~: (10, 2) ~=? Day8.safeStringToInt "10",
       "day8" ~: "safeStringToInt(123b)" ~: (123, 3) ~=? Day8.safeStringToInt "123b",
-      "day8" ~: "safeStringToInt(123b1)" ~: (123, 3) ~=? Day8.safeStringToInt "123b1"
+      "day8" ~: "safeStringToInt(123b1)" ~: (123, 3) ~=? Day8.safeStringToInt "123b1",
+      "day8" ~: "getNextToken(rect 3x2)" ~: (Rect, 5) ~=? Day8.getNextToken "rect 3x2",
+      "day8" ~: "getNextToken(3x2)" ~: (Value 3, 1) ~=? Day8.getNextToken "3x2",
+      "day8" ~: "parseToTokens(rect 3x2)" ~: [Rect, Value 3, Skip, Value 2] ~=? Day8.parseToTokens "rect 3x2" [],
+      "day8" ~: "parseToTokens(rotate column x=1 by 1)" ~: [Rotate, Column, Value 1, Skip, Value 1] ~=? Day8.parseToTokens "rotate column x=1 by 1" [],
+      "day8" ~: "parseToTokens(rotate row y=0 by 4)" ~: [Rotate, Row, Value 0, Skip, Value 4] ~=? Day8.parseToTokens "rotate row y=0 by 4" []
     ]
