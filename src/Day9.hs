cbaefb10acb4218e9d9b4f7cabbd76cdd33b1d2e
Vest <Vest@users.noreply.github.com>
2021-11-10 19:17:48 +0100

    Day 9: 120765
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,4 +1,23 @@
 module Day9 where
 
+import Debug.Trace
+
+extractInput :: String -> String
+extractInput (l : ls) | l `elem` ['A' .. 'Z'] = l : extractInput ls
+extractInput (l : ls)
+  | l == '(' =
+    let charsString = takeWhile (/= 'x') ls
+        charsNum = read charsString :: Int
+        repeatString = takeWhile (/= ')') . drop (length charsString + 1) $ ls
+        repeatNum = read repeatString :: Int
+        restString = drop (length (charsString ++ ['x'] ++ repeatString ++ [')'])) ls
+        substring = take charsNum restString
+        tailString = drop (length substring) restString
+     in (concat . replicate repeatNum $ substring) ++ extractInput tailString
+extractInput _ = ""
+
+solution1 :: String -> Int
+solution1 = length . extractInput
+
 solution :: String -> String
-solution _ = "output"
+solution = show . solution1
--- test/Days/Day9.hs
+++ test/Days/Day9.hs
@@ -6,7 +6,11 @@ import Test.HUnit
 tests' :: Test
 tests' =
   test
-    [
-    "day9" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing"
-    {- "day9" ~: "solution1(5 10 25)" ~: "test" ~=? DayX.solution1 "5 10 25" -}
+    [ "day9" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing",
+      "day9" ~: "extractInput(ADVENT)" ~: "ADVENT" ~=? Day9.extractInput "ADVENT",
+      "day9" ~: "extractInput(A(1x5)BC)" ~: "ABBBBBC" ~=? Day9.extractInput "A(1x5)BC",
+      "day9" ~: "extractInput((3x3)XYZ)" ~: "XYZXYZXYZ" ~=? Day9.extractInput "(3x3)XYZ",
+      "day9" ~: "extractInput(A(2x2)BCD(2x2)EFG)" ~: "ABCBCDEFEFG" ~=? Day9.extractInput "A(2x2)BCD(2x2)EFG",
+      "day9" ~: "extractInput((6x1)(1x3)A)" ~: "(1x3)A" ~=? Day9.extractInput "(6x1)(1x3)A",
+      "day9" ~: "extractInput(X(8x2)(3x3)ABCY)" ~: "X(3x3)ABC(3x3)ABCY" ~=? Day9.extractInput "X(8x2)(3x3)ABCY"
     ]
