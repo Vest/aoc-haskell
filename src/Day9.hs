6ce1adca1337f52a7f5a8d55dd5b1cd00cda905f
Vest <Vest@users.noreply.github.com>
2021-11-11 22:29:16 +0100

    Day 9: 120765, 11658395076
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day9.hs
+++ src/Day9.hs
@@ -22,12 +22,36 @@ solution1 = length . extractInput
 solution :: String -> String
 solution input = (show . solution1 $ input) ++ ", " ++ (show . solution2 $ input)
 
-extractIndefinitely :: String -> String
-extractIndefinitely input =
-  let extractedInput = extractInput input
-   in if input == extractedInput
-        then input
-        else extractIndefinitely extractedInput
+{- Don't need this function. It was left for our history -}
+extractInputRecursively :: String -> String
+extractInputRecursively (l : ls) | l `elem` ['A' .. 'Z'] = l : extractInputRecursively ls
+extractInputRecursively (l : ls)
+  | l == '(' =
+    let charsString = takeWhile (/= 'x') ls
+        charsNum = read charsString :: Int
+        repeatString = takeWhile (/= ')') . drop (length charsString + 1) $ ls
+        repeatNum = read repeatString :: Int
+        restString = drop (length (charsString ++ ['x'] ++ repeatString ++ [')'])) ls
+        substring = take charsNum restString
+        extractedSubstring = extractInputRecursively substring
+        tailString = drop (length substring) restString
+     in (concat . replicate repeatNum $ extractedSubstring) ++ extractInputRecursively tailString
+extractInputRecursively _ = ""
+
+getLengthRecursively :: String -> Int
+getLengthRecursively (l : ls) | l `elem` ['A' .. 'Z'] = 1 + getLengthRecursively ls
+getLengthRecursively (l : ls)
+  | l == '(' =
+    let charsString = takeWhile (/= 'x') ls
+        charsNum = read charsString :: Int
+        repeatString = takeWhile (/= ')') . drop (length charsString + 1) $ ls
+        repeatNum = read repeatString :: Int
+        restString = drop (length (charsString ++ ['x'] ++ repeatString ++ [')'])) ls
+        substring = take charsNum restString
+        extractedLength = getLengthRecursively substring
+        tailString = drop (length substring) restString
+     in repeatNum * extractedLength + getLengthRecursively tailString
+getLengthRecursively _ = 0
 
 solution2 :: String -> Int
-solution2 = length . extractIndefinitely
+solution2 = getLengthRecursively
--- test/Days/Day9.hs
+++ test/Days/Day9.hs
@@ -13,8 +13,9 @@ tests' =
       "day9" ~: "extractInput(A(2x2)BCD(2x2)EFG)" ~: "ABCBCDEFEFG" ~=? Day9.extractInput "A(2x2)BCD(2x2)EFG",
       "day9" ~: "extractInput((6x1)(1x3)A)" ~: "(1x3)A" ~=? Day9.extractInput "(6x1)(1x3)A",
       "day9" ~: "extractInput(X(8x2)(3x3)ABCY)" ~: "X(3x3)ABC(3x3)ABCY" ~=? Day9.extractInput "X(8x2)(3x3)ABCY",
-      "day9" ~: "extractIndefinitely((3x3)XYZ)" ~: "XYZXYZXYZ" ~=? Day9.extractIndefinitely "(3x3)XYZ",
-      "day9" ~: "extractIndefinitely(X(8x2)(3x3)ABCY)" ~: "XABCABCABCABCABCABCY" ~=? Day9.extractIndefinitely "X(8x2)(3x3)ABCY",
-      "day9" ~: "extractIndefinitely((27x12)(20x12)(13x14)(7x10)(1x12)A)" ~: replicate (12 * 12 * 14 * 10 * 12) 'A' ~=? Day9.extractIndefinitely "(27x12)(20x12)(13x14)(7x10)(1x12)A",
-      "day9" ~: "extractIndefinitely((25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN)" ~: 445 ~=? Day9.solution2 "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
+      "day9" ~: "extractInputRecursively((3x3)XYZ)" ~: "XYZXYZXYZ" ~=? Day9.extractInputRecursively "(3x3)XYZ",
+      "day9" ~: "extractInputRecursively(X(8x2)(3x3)ABCY)" ~: "XABCABCABCABCABCABCY" ~=? Day9.extractInputRecursively "X(8x2)(3x3)ABCY",
+      "day9" ~: "extractInputRecursively((27x12)(20x12)(13x14)(7x10)(1x12)A)" ~: replicate (12 * 12 * 14 * 10 * 12) 'A' ~=? Day9.extractInputRecursively "(27x12)(20x12)(13x14)(7x10)(1x12)A",
+      "day9" ~: "getLengthRecursively((27x12)(20x12)(13x14)(7x10)(1x12)A)" ~: (12 * 12 * 14 * 10 * 12) ~=? Day9.getLengthRecursively "(27x12)(20x12)(13x14)(7x10)(1x12)A",
+      "day9" ~: "solution2((25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN)" ~: 445 ~=? Day9.solution2 "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
     ]
