12c11b2ada9ca261a0859873bd88a0f9a2ec5c54
Vest <Vest@users.noreply.github.com>
2021-11-11 21:41:38 +0100

    Day 9: slightly updated tests
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- test/Days/Day9.hs
+++ test/Days/Day9.hs
@@ -15,6 +15,6 @@ tests' =
       "day9" ~: "extractInput(X(8x2)(3x3)ABCY)" ~: "X(3x3)ABC(3x3)ABCY" ~=? Day9.extractInput "X(8x2)(3x3)ABCY",
       "day9" ~: "extractIndefinitely((3x3)XYZ)" ~: "XYZXYZXYZ" ~=? Day9.extractIndefinitely "(3x3)XYZ",
       "day9" ~: "extractIndefinitely(X(8x2)(3x3)ABCY)" ~: "XABCABCABCABCABCABCY" ~=? Day9.extractIndefinitely "X(8x2)(3x3)ABCY",
-      "day9" ~: "extractIndefinitely((27x12)(20x12)(13x14)(7x10)(1x12)A)" ~: replicate 241920 'A' ~=? Day9.extractIndefinitely "(27x12)(20x12)(13x14)(7x10)(1x12)A",
-      "day9" ~: "extractIndefinitely((25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN)" ~: 445 ~=? (length . Day9.extractIndefinitely $ "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN")
+      "day9" ~: "extractIndefinitely((27x12)(20x12)(13x14)(7x10)(1x12)A)" ~: replicate (12 * 12 * 14 * 10 * 12) 'A' ~=? Day9.extractIndefinitely "(27x12)(20x12)(13x14)(7x10)(1x12)A",
+      "day9" ~: "extractIndefinitely((25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN)" ~: 445 ~=? Day9.solution2 "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
     ]
