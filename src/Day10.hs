8e581580fd6c58808aa19264b8d357f5a4c10686
Vest <Vest@users.noreply.github.com>
2021-11-13 19:35:12 +0100

    Day 10: Implemented simple parsing Day 9: Removed unused import.
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,4 +1,50 @@
 module Day10 where
 
+import Data.List.Split (endBy)
+import Data.Map as M
+
+data Bot = Bot
+  { low,
+    high ::
+      Int
+  }
+
+data TokenValue = IntValue Int | BotValue Int | OutputValue Int
+  deriving (Eq, Show)
+
+parseLine :: String -> Maybe [TokenValue]
+parseLine input =
+  let tokens = endBy " " input
+      tokensCount = length tokens
+   in case tokensCount of
+        6 -> Just . parseSix $ tokens
+        12 -> Just . parseTwelve $ tokens
+        _ -> Nothing
+
+parseSix :: [String] -> [TokenValue]
+parseSix input =
+  let valueString = input !! 1
+      value = read valueString :: Int
+      botString = last input
+      bot = read botString :: Int
+   in [IntValue value, BotValue bot]
+
+parseTwelve :: [String] -> [TokenValue]
+parseTwelve input =
+  let botString = input !! 1
+      bot = read botString :: Int
+      botName = head input
+      firstString = input !! 6
+      first = read firstString :: Int
+      firstName = input !! 5
+      secondString = last input
+      second = read secondString :: Int
+      secondName = input !! 10
+      createValue name value = if name == "bot" then BotValue value else OutputValue value
+   in [ createValue botName bot,
+        createValue firstName first,
+        createValue secondName second
+      ]
+
 solution :: String -> String
 solution _ = "output"
--- test/Days/Day10.hs
+++ test/Days/Day10.hs
@@ -6,7 +6,10 @@ import Test.HUnit
 tests' :: Test
 tests' =
   test
-    [
-    "day10" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing"
-    {- "day10" ~: "solution1(5 10 25)" ~: "test" ~=? Day10.solution1 "5 10 25" -}
+    [ "day10" ~: "solution1(nothing)" ~: "nothing" ~=? "nothing",
+      "day10" ~: "parseLine(value 5 goes to bot 2)" ~: Just [IntValue 5, BotValue 2] ~=? Day10.parseLine "value 5 goes to bot 2",
+      "day10" ~: "parseLine(bot 2 gives low to bot 1 and high to bot 0)" ~: Just [BotValue 2, BotValue 1, BotValue 0] ~=? Day10.parseLine "bot 2 gives low to bot 1 and high to bot 0",
+      "day10" ~: "parseLine(bot 1 gives low to output 1 and high to bot 0)" ~: Just [BotValue 1, OutputValue 1, BotValue 0] ~=? Day10.parseLine "bot 1 gives low to output 1 and high to bot 0",
+      "day10" ~: "parseLine(bot 0 gives low to output 2 and high to output 0)" ~: Just [BotValue 0, OutputValue 2, OutputValue 0] ~=? Day10.parseLine "bot 0 gives low to output 2 and high to output 0",
+      "day10" ~: "parseLine(something Wrong)" ~: Nothing ~=? Day10.parseLine "something Wrong"
     ]
