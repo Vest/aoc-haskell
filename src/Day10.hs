520fe5ed6955742819798153e15492c9b1f716ec
Vest <Vest@users.noreply.github.com>
2021-11-14 21:52:47 +0100

    Day 10: Added parsing
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,13 +1,18 @@
 module Day10 where
 
 import Data.List.Split (endBy)
-import Data.Map as M
+import qualified Data.Map as M
+import Data.Maybe (mapMaybe)
 
 data Bot = Bot
   { low,
     high ::
-      Int
+      ValueLocation
   }
+  deriving (Eq, Show)
+
+data ValueLocation = BotLocation Int | OutputLocation Int
+  deriving (Eq, Show)
 
 data TokenValue = IntValue Int | BotValue Int | OutputValue Int
   deriving (Eq, Show)
@@ -45,6 +50,36 @@ parseTwelve input =
         createValue firstName first,
         createValue secondName second
       ]
+
+parseInput :: String -> (M.Map Int Bot, M.Map Int ValueLocation)
+parseInput =
+  foldl
+    ( \(rules, values) tokens ->
+        let tokensCount = length tokens
+         in if tokensCount == 2
+              then (rules, updateValuesMap values tokens)
+              else (updateBotsMap rules tokens, values)
+    )
+    (M.empty, M.empty)
+    . mapMaybe parseLine
+    . lines
+
+updateValuesMap :: M.Map Int ValueLocation -> [TokenValue] -> M.Map Int ValueLocation
+updateValuesMap m [IntValue i, BotValue b] = M.insert i (BotLocation b) m
+updateValuesMap m _ = m
+
+updateBotsMap :: M.Map Int Bot -> [TokenValue] -> M.Map Int Bot
+updateBotsMap m [BotValue b, l, h] =
+  let lowMaybe = extractLocation l
+      highMaybe = extractLocation h
+      extractLocation v = case v of
+        BotValue bv -> Just . BotLocation $ bv
+        OutputValue ov -> Just . OutputLocation $ ov
+        _ -> Nothing
+   in case (lowMaybe, highMaybe) of
+        (Just lv, Just hv) -> M.insert b Bot {low = lv, high = hv} m
+        _ -> m
+updateBotsMap m _ = m
 
 solution :: String -> String
 solution _ = "output"
--- test/Days/Day10.hs
+++ test/Days/Day10.hs
@@ -1,5 +1,6 @@
 module Days.Day10 where
 
+import qualified Data.Map as M
 import Day10
 import Test.HUnit
 
@@ -11,5 +12,17 @@ tests' =
       "day10" ~: "parseLine(bot 2 gives low to bot 1 and high to bot 0)" ~: Just [BotValue 2, BotValue 1, BotValue 0] ~=? Day10.parseLine "bot 2 gives low to bot 1 and high to bot 0",
       "day10" ~: "parseLine(bot 1 gives low to output 1 and high to bot 0)" ~: Just [BotValue 1, OutputValue 1, BotValue 0] ~=? Day10.parseLine "bot 1 gives low to output 1 and high to bot 0",
       "day10" ~: "parseLine(bot 0 gives low to output 2 and high to output 0)" ~: Just [BotValue 0, OutputValue 2, OutputValue 0] ~=? Day10.parseLine "bot 0 gives low to output 2 and high to output 0",
-      "day10" ~: "parseLine(something Wrong)" ~: Nothing ~=? Day10.parseLine "something Wrong"
+      "day10" ~: "parseInput(sample)"
+        ~: ( M.fromList
+               [ (0, Bot {low = OutputLocation 2, high = OutputLocation 0}),
+                 (1, Bot {low = OutputLocation 1, high = BotLocation 0}),
+                 (2, Bot {low = BotLocation 1, high = BotLocation 0})
+               ],
+             M.fromList
+               [ (5, BotLocation 2),
+                 (3, BotLocation 1),
+                 (2, BotLocation 2)
+               ]
+           )
+        ~=? Day10.parseInput "value 5 goes to bot 2\nbot 2 gives low to bot 1 and high to bot 0\nvalue 3 goes to bot 1\nbot 1 gives low to output 1 and high to bot 0\nbot 0 gives low to output 2 and high to output 0\nvalue 2 goes to bot 2"
     ]
